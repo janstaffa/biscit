1d2788b5f3fc442d0159dfbd7914a21c8cdd3764
Jan Šfaffa <agenttupesy@gmail.com>
2021-03-30 16:52:58 +0200

    postgres db setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
-import express from 'express';
 require('dotenv-safe').config();
+import express from 'express';
+import path from 'path';
+import { createConnection } from 'typeorm';
 
 const app = express();
 
 app.get('/', (_, res) => {
   res.send('Hey from the server!');
 });
-app.listen(process.env.PORT, () => {
-  console.log(`server running on port ${process.env.PORT}`);
-});
+
+(async () => {
+  const conn = await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: true,
+    migrations: [path.join(__dirname, './migrations/*')],
+    entities: [path.join(__dirname, './entities/*')],
+    synchronize: true,
+  });
+  await conn.runMigrations();
+
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+})().catch((err) => console.error(err));
--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
+    DATABASE_URL: string;
   }
 }
