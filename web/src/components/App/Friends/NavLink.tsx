3279139ba5a87ddfcc5d8cecb41ce50e901d42e0
Jan Šfaffa <agenttupesy@gmail.com>
2021-04-15 13:09:44 +0200

    updated friends nav bar

--- web/src/components/App/Friends/NavLink.tsx
+++ web/src/components/App/Friends/NavLink.tsx
@@ -1,23 +1,27 @@
 import Link from 'next/link';
-import { ReactNode } from 'react';
-export interface NavLinkProps {
+import { InputHTMLAttributes, ReactNode } from 'react';
+export type NavLinkProps = InputHTMLAttributes<HTMLDivElement> & {
   children: ReactNode;
   href: string;
   active?: boolean;
-}
+};
 
 const NavLink: React.FC<NavLinkProps> = ({
   children,
   href,
   active = false,
+  ...props
 }) => {
   return (
     <Link href={href}>
       <div
         className={
-          'text-light-200 font-roboto mx-2 px-3 rounded-sm cursor-pointer' +
-          (active ? ' bg-dark-50' : ' hover:bg-dark-100')
+          props.className
+            ? ` ${props.className}`
+            : 'text-light-200 font-bold mx-2 px-3 rounded-sm cursor-pointer' +
+              (active ? ' bg-dark-50' : ' hover:bg-dark-100')
         }
+        {...props}
       >
         {children}
       </div>
