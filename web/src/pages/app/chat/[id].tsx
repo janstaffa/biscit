04bd5c56e33b73df3890caef8a0484794b547229
Jan Šfaffa <agenttupesy@gmail.com>
2021-05-06 20:09:01 +0200

    added message caching, fixed scrolling and more bugs

--- web/src/pages/app/chat/[id].tsx
+++ web/src/pages/app/chat/[id].tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect, useRef, useState } from 'react';
 import { FaHashtag } from 'react-icons/fa';
+import { useQuery } from 'react-query';
 import ClipLoader from 'react-spinners/ClipLoader';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 import { IncomingSocketChatMessage } from '../../..';
@@ -13,15 +14,19 @@ import Layout from '../../../components/App/Layout';
 import { genericErrorMessage } from '../../../constants';
 import {
   MessageSnippetFragment,
+  ThreadMessagesDocument,
+  ThreadMessagesQuery,
+  ThreadMessagesQueryVariables,
   ThreadMessagesResponse,
-  useThreadMessagesQuery,
   useThreadQuery
 } from '../../../generated/graphql';
 import { socket } from '../../../utils/createWSconnection';
 import { isServer } from '../../../utils/isServer';
 import { errorToast } from '../../../utils/toasts';
+import { useGQLRequest } from '../../../utils/useGQLRequest';
 import withAuth from '../../../utils/withAuth';
 
+const messagesLimit = 30;
 const Chat: NextPage = () => {
   const router = useRouter();
   if (!router.query.id) {
@@ -49,16 +54,21 @@ const Chat: NextPage = () => {
   );
 
   const [cursor, setCursor] = useState<string | null>(null);
-  const { data: incomingThreadMessages, refetch: refetchThreadMessages } = useThreadMessagesQuery(
-    {
-      options: { threadId, limit: 30, cursor }
-    },
+  const variables = {
+    options: {
+      threadId,
+      limit: messagesLimit,
+      cursor
+    }
+  } as ThreadMessagesQueryVariables;
+  const { data: incomingThreadMessages, refetch: refetchThreadMessages } = useQuery<ThreadMessagesQuery>(
+    [`ThreadMessages-${threadId}`, variables],
+    useGQLRequest<ThreadMessagesQuery, ThreadMessagesQueryVariables>(ThreadMessagesDocument).bind(null, variables),
     {
       onError: (err) => {
         console.error(err);
         errorToast(genericErrorMessage);
-      },
-      cacheTime: 0
+      }
     }
   );
 
@@ -73,7 +83,7 @@ const Chat: NextPage = () => {
     const feed = document.querySelector('#chat-feed')! as HTMLDivElement;
     if (!feed) return;
 
-    console.log(feed.scrollTop, feed.scrollHeight);
+    console.log(feed.scrollTop, feed.scrollHeight, feed);
     feed.scrollTop = px === 0 ? feed.scrollHeight : px;
     console.log(feed.scrollTop, feed.scrollHeight);
   };
@@ -97,6 +107,7 @@ const Chat: NextPage = () => {
     setMessages([]);
     setMoreMessages(false);
     setIsLoadingMessages(false);
+    setCursor(null);
     loadMessages();
 
     const handleMessage = (e) => {
@@ -149,20 +160,33 @@ const Chat: NextPage = () => {
       return;
     }
     if (incomingMessages) {
+      console.log('incmonigMessages', incomingMessages);
       const prevMessagesLength = messagesRef.current.length;
+      console.log('currentMessages', messagesRef.current);
       const newMessages = [...incomingMessages, ...messagesRef.current];
       console.log('setting messages state', newMessages);
       setMessages(newMessages);
       setMoreMessages(hasMore);
       setIsLoadingMessages(false);
 
-      scroll(prevMessagesLength === 0 ? 0 : incomingMessages.length * 84);
+      console.log('prev scroll');
+      console.log('after scroll');
     }
   }, [incomingThreadMessages]);
 
   useEffect(() => {
     const feed = document.querySelector('#chat-feed')! as HTMLDivElement;
     if (moreMessages) {
+      if (messages.length <= messagesLimit) {
+        scroll(0);
+      } else {
+        const remainder = messages.length % messagesLimit;
+        if (remainder === 0) {
+          scroll(messagesLimit * 84);
+        } else {
+          scroll(remainder * 84);
+        }
+      }
       feed.onscroll = () => {
         if (feed.scrollTop === 0) {
           setCursor(messages[0].createdAt);
@@ -175,6 +199,9 @@ const Chat: NextPage = () => {
     }
   }, [moreMessages, messages]);
 
+  useEffect(() => {
+    console.log('isLoading change', isLoadingMessages);
+  }, [isLoadingMessages]);
   return (
     <>
       <Head>
