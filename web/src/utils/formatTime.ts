bdb207368444121106ee242c73ee8ba84390abba
Jan Šfaffa <agenttupesy@gmail.com>
2021-05-10 10:19:35 +0200

    better formatTime function

--- web/src/utils/formatTime.ts
+++ web/src/utils/formatTime.ts
@@ -1,10 +1,19 @@
-export const formatTime = (time: string) => {
+interface options {
+  fullDate?: boolean;
+}
+
+export const formatTime = (time: string, options: options = { fullDate: false }) => {
   let date = new Date(time);
   if (Number.isNaN(Date.parse(time))) {
     date = new Date(parseInt(time));
   }
   const today = new Date();
 
+  if (options.fullDate) {
+    return `${date.getDate()}.${date.getMonth()} ${date.getFullYear()} ${('00' + date.getHours()).slice(-2)}:${(
+      '00' + date.getMinutes()
+    ).slice(-2)}:${('00' + date.getSeconds()).slice(-2)}`;
+  }
   if (date.getDate() !== today.getDate()) {
     if (date.getFullYear() !== today.getFullYear()) {
       return `${date.getDate()}.${date.getMonth()}. ${date.getFullYear()}`;
