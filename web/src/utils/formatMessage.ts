34707d94a82250e8d459b652a4e14cf03c1c031d
Jan Šfaffa <agenttupesy@gmail.com>
2021-05-24 22:48:04 +0200

    added XSS string escape protection

--- web/src/utils/formatMessage.ts
+++ web/src/utils/formatMessage.ts
@@ -1,28 +1,29 @@
 import { urlRegExp } from '../constants';
+import { escapeString } from './escapeString';
 
 export const formatMessage = (content: string): string => {
   if (urlRegExp.test(content)) {
     return content.replace(urlRegExp, (url) => {
-      return `<a href="${url}" target="_blank">${url}</a>`;
+      return `<a href="${url}" target="_blank">${escapeString(url)}</a>`;
     });
   }
   if (/\*\*\*(.*?)\*\*\*/.test(content)) {
     return content.replace(/\*\*\*(.*?)\*\*\*/, (inner) => {
       const realContent = inner.substring(3, inner.length - 3);
-      return `<b><i>${realContent}</i></b>`;
+      return `<b><i>${escapeString(realContent)}</i></b>`;
     });
   }
   if (/\*\*(.*?)\*\*/.test(content)) {
     return content.replace(/\*\*(.*?)\*\*/, (inner) => {
       const realContent = inner.substring(2, inner.length - 2);
-      return `<i>${realContent}</i>`;
+      return `<i>${escapeString(realContent)}</i>`;
     });
   }
   if (/\*(.*?)\*/.test(content) && !/\*\*(.*?)\*\*/.test(content)) {
     return content.replace(/\*(.*?)\*/, (inner) => {
       const realContent = inner.substring(1, inner.length - 1);
-      return `<b>${realContent}</b>`;
+      return `<b>${escapeString(realContent)}</b>`;
     });
   }
-  return content;
+  return escapeString(content);
 };
